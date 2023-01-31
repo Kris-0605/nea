6e1868795f3caf671852592c90157da83403bf11
Christopher Ashford <82324669+Kris-0605@users.noreply.github.com>
2023-01-31 14:26:46 +0000

    Clean up clutter in exercise.py

--- game/main.py
+++ game/main.py
@@ -1,3 +1,3 @@
 from kris_engine import Engine
-from exercise import Exercise
-e = Engine(scene=Exercise, width=(320/224)*720, height=720)
+from exercise import ExerciseClassic
+e = Engine(scene=ExerciseClassic, width=(320/224)*720, height=720)
